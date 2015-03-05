0c74d8486dbc99db7226b15486a4f27135ad56e8
Kristian <ktslabbie@gmail.com>
2015-03-05 20:45:42 +0900

    Added CORS headers to webserver.

--- twitter/src/main/java/jp/titech/twitter/web/TwontoApplication.java
+++ twitter/src/main/java/jp/titech/twitter/web/TwontoApplication.java
@@ -1,5 +1,10 @@
 package jp.titech.twitter.web;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
+
 import jp.titech.twitter.config.Configuration;
 import jp.titech.twitter.db.TweetBase;
 import jp.titech.twitter.mining.api.TwitterAPIAccountManager;
@@ -8,6 +13,7 @@ import jp.titech.twitter.util.Util;
 import jp.titech.twitter.util.Vars;
 
 import org.apache.log4j.PropertyConfigurator;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
@@ -27,6 +33,16 @@ public class TwontoApplication extends Application<TwontoConfiguration> {
 		Util.loadStopwords(Vars.STOPWORDS_FILE);
 	}
 
+	private void configureCORS(Environment environment) {
+		Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+		filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+		filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+		filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+		filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+		filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+		filter.setInitParameter("allowCredentials", "true");
+	}
+
 	@Override
 	public void run(TwontoConfiguration configuration, Environment environment) {
 		final SimpleUserResource simpleUserResource = new SimpleUserResource(configuration.getDefaultName());
@@ -34,6 +50,8 @@ public class TwontoApplication extends Application<TwontoConfiguration> {
 		final UserFollowersListResource userFollowersListResource = new UserFollowersListResource(configuration.getDefaultName());
 		final KeywordUserListResource keywordUserListResource = new KeywordUserListResource(configuration.getDefaultKeyword());
 		final APIHealthCheck healthCheck = new APIHealthCheck();
+
+		configureCORS(environment);
 		
 		environment.jersey().register(simpleUserResource);
 		environment.jersey().register(userResource);
