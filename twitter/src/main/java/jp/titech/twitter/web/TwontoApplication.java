c5da55abcabdba10a885692fa206831205762232
ktslabbie <ktslabbie@gmail.com>
2015-04-01 14:42:23 +0900

    Stricter CORS - allow only github.io

--- twitter/src/main/java/jp/titech/twitter/web/TwontoApplication.java
+++ twitter/src/main/java/jp/titech/twitter/web/TwontoApplication.java
@@ -35,8 +35,8 @@ public class TwontoApplication extends Application<TwontoConfiguration> {
 	private void configureCORS(Environment environment) {
 		Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 		filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
-		filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
-		filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+		filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET");
+		filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "http://ktslabbie.github.io");
 		filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
 		filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
 		filter.setInitParameter("allowCredentials", "true");
